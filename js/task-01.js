f0c04bf62043ded6a6632e903eb3e407e9a68172
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-03 07:49:01 +0200

    task-1 modified task-2

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,20 @@
-const numberOfCategories = document.querySelectorAll(".item").length;
-console.dir(`Number of categories: ${numberOfCategories}`);
+const numberOfCategories = document.querySelectorAll(".item");
+console.dir(`Number of categories: ${numberOfCategories.length}`);
 
-const categories = Array.from(document.querySelectorAll(".item h2"));
-const elements = Array.from(document.querySelectorAll(".item ul"));
+// const categories = Array.from(document.querySelectorAll(".item h2"));
+// const elements = Array.from(document.querySelectorAll(".item ul"));
 
-const category = categories.map((category) => category.innerHTML);
-const quantityEl = elements.map((element) => element.children.length);
+// const category = categories.map((category) => category.innerHTML);
+// const quantityEl = elements.map((element) => element.children.length);
 
-for (let i = 0; i < category.length; i += 1) {
-  console.dir(`Category: ${category[i]}`);
-  console.dir(`Elements: ${quantityEl[i]}`);
-}
+// for (let i = 0; i < category.length; i += 1) {
+//   console.dir(`Category: ${category[i]}`);
+//   console.dir(`Elements: ${quantityEl[i]}`);
+// }
+
+numberOfCategories.forEach((element) => {
+  const category = element.querySelector("h2");
+  console.log(`Category: ${category.textContent}`);
+  const elements = element.querySelectorAll("li");
+  console.log(`Elements: ${elements.length}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,36 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+// const listofIngredients = document.querySelector("#ingredients");
+// const itemArray = [];
+
+// function createItem() {
+//   for (let i = 0; i < ingredients.length; i += 1) {
+//     const li = document.createElement("li");
+//     li.textContent = ingredients[i];
+//     li.classList.add("item");
+//     itemArray.push(li);
+//   }
+//   listofIngredients.append(...itemArray);
+// }
+
+// createItem();
+
+const listofIngredients = document.querySelector("#ingredients");
+
+// const items = ingredients.map(function (ingredient) {
+//   const item = document.createElement("li");
+//   item.textContent = ingredient;
+//   item.className = "item";
+//   return item;
+// });
+
+const items = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.className = "item";
+  return item;
+});
+
+listofIngredients.append(...items);
