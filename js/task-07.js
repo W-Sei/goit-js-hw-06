e2ab8d2f1970659b117b4a1af9582196eb6ba4c2
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-03 10:25:17 +0200

    task-7 and task-8

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,11 @@
+const input = document.querySelector("#font-size-control");
+const article = document.querySelector("#text");
 
+input.addEventListener("input", changeArticlaeFontSize);
+
+let articleFontSize = "";
+
+function changeArticlaeFontSize() {
+  articleFontSize = `<style>#text{font-size: ${input.value}px;}</style>`;
+  input.insertAdjacentHTML("beforebegin", articleFontSize);
+}
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const submitForm = document.querySelector(".login-form");
+submitForm.addEventListener("submit", onFormSybmission);
 
+function onFormSybmission(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+
+  if (!formEl.email.value || !formEl.password.value) {
+    alert("Plese, kindly fill in all the form fields. Thank you!");
+  }
+
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  submitForm.reset();
+}
