e1b40593989d3c2784f0635d9f6d541864ec491d
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-03 09:53:49 +0200

    task-4 and task-5

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,19 @@
+let counterValue = 0;
 
+const pressBtnDecrement = document.querySelector(`[data-action = "decrement"]`);
+const pressBtnIncrement = document.querySelector(`[data-action = "increment"]`);
+
+const value = document.querySelector("#value");
+
+pressBtnDecrement.addEventListener("click", decrementClick);
+pressBtnIncrement.addEventListener("click", incrementClick);
+
+function incrementClick() {
+  counterValue += 1;
+  value.textContent = counterValue;
+}
+
+function decrementClick() {
+  counterValue -= 1;
+  value.textContent = counterValue;
+}
--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,11 @@
+const userName = document.querySelector(`#name-output`);
+const input = document.querySelector(`#name-input`);
 
+input.addEventListener("input", changeName);
+
+function changeName() {
+  if ((userName.textContent = input.value)) {
+    return input.value;
+  }
+  return (userName.textContent = "Anonymous");
+}
