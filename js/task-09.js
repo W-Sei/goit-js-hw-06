e11120e9f2b5d25ab39a6e7e6f32ab13534e58f6
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-03 10:37:56 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const docStyle = document.querySelector("style");
+const spanColor = document.querySelector(".color");
+const changeButtonColor = document.querySelector(".change-color");
+changeButtonColor.addEventListener("click", bodyColorRandomizer);
+
+let randomBodyColor = "";
+
+function bodyColorRandomizer() {
+  randomBodyColor = `body{background-color: ${getRandomHexColor()};}`;
+
+  docStyle.insertAdjacentHTML("beforeend", randomBodyColor);
+  spanColor.textContent = getRandomHexColor();
+}
+
+console.dir(docStyle);
