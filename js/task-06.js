501f6d811ddbb0a1ffcd1867d0c68d3061ee2161
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-03 10:08:32 +0200

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,17 @@
+const input = document.querySelector("#validation-input");
+console.log(input);
 
+input.addEventListener("blur", checkNumberOfSymbols);
+
+// function checkNumberOfSymbols() {
+//   if (input.value.length !== Number(input.dataset.length)) {
+//     input.classList.add("invalid");
+//   } else input.classList.replace("invalid", "valid");
+// }
+
+function checkNumberOfSymbols() {
+  if (input.value.length !== Number(input.dataset.length)) {
+    return input.classList.add("invalid");
+  }
+  return input.classList.replace("invalid", "valid");
+}
