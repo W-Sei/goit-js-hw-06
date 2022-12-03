83c39e4fd2dacf69342eb069d63f88de24e57ef5
Hanna Franko <107546169+W-Sei@users.noreply.github.com>
2022-12-03 13:16:16 +0200

    task-9 revised

--- js/task-09.js
+++ js/task-09.js
@@ -4,18 +4,28 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const docStyle = document.querySelector("style");
-const spanColor = document.querySelector(".color");
+// const docStyle = document.querySelector("style");
+// const spanColor = document.querySelector(".color");
+// const changeButtonColor = document.querySelector(".change-color");
+// changeButtonColor.addEventListener("click", bodyColorRandomizer);
+
+// let randomBodyColor = "";
+
+// function bodyColorRandomizer() {
+//   randomBodyColor = `body{background-color: ${getRandomHexColor()};}`;
+
+//   docStyle.insertAdjacentHTML("beforeend", randomBodyColor);
+//   spanColor.textContent = getRandomHexColor();
+// }
+
+// console.dir(docStyle);
+
 const changeButtonColor = document.querySelector(".change-color");
-changeButtonColor.addEventListener("click", bodyColorRandomizer);
+const bodyColor = document.querySelector("body");
+const spanColor = document.querySelector(".color");
 
-let randomBodyColor = "";
-
-function bodyColorRandomizer() {
-  randomBodyColor = `body{background-color: ${getRandomHexColor()};}`;
-
-  docStyle.insertAdjacentHTML("beforeend", randomBodyColor);
-  spanColor.textContent = getRandomHexColor();
-}
-
-console.dir(docStyle);
+changeButtonColor.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  spanColor.textContent = color;
+});
